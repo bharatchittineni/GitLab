5c44e5c7871eb1b2bb4ce9df847280424901712c
bharatchittineni <bharatchittineni@gmail.com>
2017-01-10 17:51:38 +0530

    initialisation

---
+++ GitLab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLab
+{
+    public class Program
+    {
+        private Int32 Wow =10;
+    }
+}
